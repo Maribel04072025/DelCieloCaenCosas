967657f7a2a99e26c6037232b1559de40771aa17
Maribel Ceballos <maribel.ceballosc@autonoma.edu.co>
2025-05-18 19:05:01 -0500

    se sube documentacion de las clases

--- src/autonoma/DelCieloCaenCosas/modelo/Comida.java
+++ src/autonoma/DelCieloCaenCosas/modelo/Comida.java
@@ -4,17 +4,34 @@
  */
 package autonoma.DelCieloCaenCosas.modelo;
 
-/**
- *
- * @author marib
- */
 import java.awt.Image;
 
+/**
+ * Representa un objeto de tipo Comida que cae desde la parte superior de la pantalla.
+ * Esta clase extiende la clase {@link ObjetoQueCae} e indica que otorga puntos al jugador.
+ * 
+ * @author maribel ceballos
+ * @version 20250518
+ * @since 1.0
+ */
 public class Comida extends ObjetoQueCae {
+
+    /**
+     * Crea una nueva instancia de Comida.
+     *
+     * @param x        Posición horizontal inicial del objeto.
+     * @param velocidad Velocidad de caída del objeto.
+     * @param imagen   Imagen que representa visualmente la comida.
+     */
     public Comida(int x, int velocidad, Image imagen) {
         super(x, velocidad, imagen);
     }
 
+    /**
+     * Retorna la cantidad de puntos que otorga este objeto al jugador.
+     * 
+     * @return 1 punto por cada comida recogida.
+     */
     @Override
     public int getPuntos() {
         return 1;
--- src/autonoma/DelCieloCaenCosas/modelo/Veneno.java
+++ src/autonoma/DelCieloCaenCosas/modelo/Veneno.java
@@ -4,17 +4,34 @@
  */
 package autonoma.DelCieloCaenCosas.modelo;
 
-/**
- *
- * @author marib
- */
 import java.awt.Image;
 
+/**
+ * Representa un objeto de tipo Veneno que cae desde la parte superior de la pantalla.
+ * Esta clase extiende la clase {@link ObjetoQueCae} e indica que penaliza al jugador con puntos negativos.
+ * 
+ * @author maribel ceballos 
+ * @version 20250518
+ * @since 1.0
+ */
 public class Veneno extends ObjetoQueCae {
+
+    /**
+     * Crea una nueva instancia de Veneno.
+     *
+     * @param x         Posición horizontal inicial del objeto.
+     * @param velocidad Velocidad de caída del objeto.
+     * @param imagen    Imagen que representa visualmente el veneno.
+     */
     public Veneno(int x, int velocidad, Image imagen) {
         super(x, velocidad, imagen);
     }
 
+    /**
+     * Retorna la cantidad de puntos que afecta este objeto al jugador.
+     * 
+     * @return -2 puntos por cada veneno recogido.
+     */
     @Override
     public int getPuntos() {
         return -2;
